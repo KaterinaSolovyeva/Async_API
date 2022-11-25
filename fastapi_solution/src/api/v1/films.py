2bc8b8c796011c7eb260c182fe667cea11c1fd6c
KaterinaSolovyeva <aka-malta@yandex.ru>
2022-11-25 18:12:20 +0300

    Get movies with filter and sorting from ES

--- fastapi_solution/src/api/v1/films.py
+++ fastapi_solution/src/api/v1/films.py
@@ -1,28 +1,49 @@
 from http import HTTPStatus
+from typing import Optional
 
-from fastapi import APIRouter, Depends, HTTPException
+from core.config import DEFAULT_PAGE_NUMBER, DEFAULT_PAGE_SIZE
+from fastapi import APIRouter, Depends, HTTPException, Query
+from models.film import ESFilm, Film, FilmDetailed
 from pydantic import BaseModel
 from services.film import FilmService, get_film_service
 
 router = APIRouter()
 
 
-class Film(BaseModel):
-    id: str
-    title: str
-
+@router.get(
+    '/',
+    response_model=list[Film],
+    summary='All movies',
+    description='Returns all filmworks'
+)
+async def get_all_filmworks(
+    film_service: FilmService = Depends(get_film_service),
+    sort: str = Query('', description='Sorting'),
+    genre: str = Query(None, description='Filter by genre uuid', alias='filter[genre]'),
+    page_size: int = Query(DEFAULT_PAGE_SIZE, description='Number of filmworks on page', alias='page[size]'),
+    page: int = Query(DEFAULT_PAGE_NUMBER, description='Page number', alias='page[number]')
+):
+    """Returns all filmworks."""
+    films = await film_service.get_all_films_from_elastic(
+        page_size=page_size,
+        page=page,
+        sort=sort,
+        genre=genre
+    )
+    return films
 
 @router.get(
     '/{film_id}',
-    response_model=Film,
+    response_model=FilmDetailed,
     summary='Information about the film',
     description='Returns information about a movie by its id',
 )
 async def film_details(
     film_id: str,
     film_service: FilmService = Depends(get_film_service)
-) -> Film:
+) -> FilmDetailed:
+    """Returns filmwork's detailed description."""
     film = await film_service.get_by_id(film_id)
     if not film:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='Film not found')
-    return Film(id=film.id, title=film.title)
+    return FilmDetailed(uuid=film_id, **film.dict(by_alias=True))
--- fastapi_solution/src/services/film.py
+++ fastapi_solution/src/services/film.py
@@ -1,12 +1,14 @@
+# from elasticsearch_dsl import Q, Search
 from functools import lru_cache
 from typing import Optional
 
 from aioredis import Redis
+from core.config import DEFAULT_PAGE_NUMBER, DEFAULT_PAGE_SIZE
 from db.elastic import get_elastic
 from db.redis import get_redis
 from elasticsearch import AsyncElasticsearch, NotFoundError
 from fastapi import Depends
-from models.film import Film
+from models.film import ESFilm, Film
 
 FILM_CACHE_EXPIRE_IN_SECONDS = 60 * 5
 
@@ -15,15 +17,52 @@ class FilmService:
     def __init__(self, redis: Redis, elastic: AsyncElasticsearch):
         self.redis = redis
         self.elastic = elastic
+    
+    async def get_all_films_from_elastic(self, **params):
+        page_size = params.get('page_size', DEFAULT_PAGE_SIZE)
+        page = params.get('page', DEFAULT_PAGE_NUMBER)
+        sort = params.get('sort', '')
+        genre = params.get('genre', None)
+        body = None
+        if sort and str(sort)[0] == '-':
+            sort = str(sort)[1:]+':desc'
+        if genre:
+            body = {
+                'query': {
+                    'nested': {
+                        'path': "genres",
+                        'query': {
+                            'bool': {
+                                'must': [
+                                    {
+                                        'match': {
+                                            'genres.id': genre
+                                        }
+                                    }
+                                ]
+                            }
+                        }
+                    }
+                }
+            }
+        films = await self.elastic.search(
+            index='movies',
+            body=body,
+            params={
+                'size': page_size,
+                'from': page - 1,
+                'sort': sort
+            }
+        )
+        return [Film(uuid=doc['_id'], **doc['_source']).dict() for doc in films['hits']['hits']]
 
-    async def get_by_id(self, film_id: str) -> Optional[Film]:
+    async def get_by_id(self, film_id: str) -> Optional[ESFilm]:
         film = await self._film_from_cache(film_id)
         if not film:
             film = await self._get_film_from_elastic(film_id)
             if not film:
                 return None
             await self._put_film_to_cache(film)
-
         return film
 
     async def _get_film_from_elastic(self, film_id: str) -> Optional[Film]:
@@ -31,17 +70,17 @@ class FilmService:
             doc = await self.elastic.get('movies', film_id)
         except NotFoundError:
             return None
-        return Film(**doc['_source'])
+        return ESFilm(uuid=['_id'], **doc['_source'])
 
-    async def _film_from_cache(self, film_id: str) -> Optional[Film]:
+    async def _film_from_cache(self, film_id: str) -> Optional[ESFilm]:
         data = await self.redis.get(film_id)
         if not data:
             return None
-        film = Film.parse_raw(data)
+        film = ESFilm.parse_raw(data)
         return film
 
-    async def _put_film_to_cache(self, film: Film):
-        await self.redis.set(film.id, film.json(), expire=FILM_CACHE_EXPIRE_IN_SECONDS)
+    async def _put_film_to_cache(self, film: ESFilm):
+        await self.redis.set(film.uuid, film.json(by_alias=True), expire=FILM_CACHE_EXPIRE_IN_SECONDS)
 
 
 @lru_cache()

a14e7f943b84d9cd7cc5674b02ffce7945c2ac23
Vadim.gartman <v.gartman@qiwi.com>
2022-11-30 09:33:04 +0300

    fastapi revived

--- fastapi_solution/src/app/connections/elastic.py
+++ fastapi_solution/src/app/connections/elastic.py
@@ -3,12 +3,10 @@ from typing import Optional
 from elasticsearch import AsyncElasticsearch
 
 from app.core.config import settings
-from etl_service.etl.helpers import backoff
 
 es: Optional[AsyncElasticsearch] = None
 
 
-@backoff(border_sleep_time=15)
 async def get_es_connection():
     """Функция для установления соединения с es"""
     return AsyncElasticsearch(

70e342105fced31f537c77eaa0fe92c93bc5dccc
KaterinaSolovyeva <aka-malta@yandex.ru>
2022-12-01 00:01:20 +0300

    Added readme.md for review

--- fastapi_solution/src/app/serializers/query_params_classes.py
+++ fastapi_solution/src/app/serializers/query_params_classes.py
@@ -6,7 +6,7 @@ from app.core.config import settings
 class PaginationDataParams:
     def __init__(
         self,
-        sort: str = Query('', description='Sorting'),
+        sort: str = Query('', description='Sorting by imdb_rating', regex='^-?imdb_rating$'),
         page_size: int = Query(settings.DEFAULT_PAGE_SIZE, description='Number of filmworks on page',
                                alias='page[size]', ge=1),
         page: int = Query(settings.DEFAULT_PAGE_NUMBER, description='Page number', alias='page[number]', ge=0)

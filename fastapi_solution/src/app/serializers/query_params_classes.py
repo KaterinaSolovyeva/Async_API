2828b124d7e84d89b571451911516b5fe96803d0
KaterinaSolovyeva <aka-malta@yandex.ru>
2022-12-05 14:50:13 +0300

    Update query_params_classes.py

--- fastapi_solution/src/app/serializers/query_params_classes.py
+++ fastapi_solution/src/app/serializers/query_params_classes.py
@@ -9,7 +9,7 @@ class PaginationDataParams:
         sort: str = Query('', description='Sorting by imdb_rating', regex='^-?imdb_rating$'),
         page_size: int = Query(settings.DEFAULT_PAGE_SIZE, description='Number of filmworks on page',
                                alias='page[size]', ge=1),
-        page: int = Query(settings.DEFAULT_PAGE_NUMBER, description='Page number', alias='page[number]', ge=0)
+        page: int = Query(settings.DEFAULT_PAGE_NUMBER, description='Page number', alias='page[number]', ge=1)
     ):
         self.sort = sort
         self.page_size = page_size
